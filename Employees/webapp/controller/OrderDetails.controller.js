2e48592e0589791a83d06e65132585445aa139d2
sschacon <sschacon@gmail.com>
2021-05-11 18:56:31 +0000

    GIT Comment

--- Employees/webapp/controller/OrderDetails.controller.js
+++ Employees/webapp/controller/OrderDetails.controller.js
@@ -4,10 +4,18 @@ sap.ui.define([
 ],
     function (Controller, History) {
 
+        function _onObjectMatched(oEvent) {
+            this.getView().bindElement({
+                path: "/Orders(" + oEvent.getParameter("arguments").OrderID + ")",
+                model: "odataNorthwind"
+            })
+        };
+
         return Controller.extend("logaligroup.Employees.controller.OrderDetails", {
 
             onInit: function () {
-
+                var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+                oRouter.getRoute("RouteOrderDetails").attachPatternMatched(_onObjectMatched, this);
             },
 
             onBack: function (oEvent) {
